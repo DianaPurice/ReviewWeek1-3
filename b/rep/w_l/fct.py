a7ba01ac49593edf6f30c180ecbd4d86b40c8589
repl.it user <replituser@example.com>
2021-02-28 08:13:09 +0000

    sa

---
+++ b/rep/w_l/fct.py
@@ -0,0 +1,12 @@
+# display message
+print("Please enter a number:")
+n = int(input())
+
+# calculate factorial
+c = 0
+total = 1
+while (c < n):
+  c = c +1
+  total = total * c
+# display factorial
+print("The factorial is", total)
