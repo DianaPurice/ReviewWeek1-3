685c0387af3aec4d0db93de261addd54cc648197
repl.it user <replituser@example.com>
2021-02-28 04:13:01 +0000

    Updated code to display a longer multi-line message.

--- b/out/m_m.py
+++ b/out/m_m.py
@@ -1,3 +1,20 @@
 # Display message to the standard outpu
 print("System Failure Imminent!")
 print("System reboot has been initiated...")
+print()
+print("...rebooting sensory system")
+print("...rebooting output motors")
+print("...rebooting hover engine")
+print()
+print("System online.")
+print("\nsecond version\n")
+print("System Failure Imminent!\nSystem reboot has been initiated...\n\n...rebooting sensory system\n...rebooting output motors\n...rebooting hover engine\n\nSystem online.")
+print("\nthird version\n")
+print("""System Failure Imminent!
+System reboot has been initiated...
+
+...rebooting sensory system
+...rebooting output motors
+...rebooting hover engine
+
+System online.""")
