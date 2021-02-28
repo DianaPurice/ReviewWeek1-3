dc039d76841457c6f75e56b95ce8cb6a5d36e825
repl.it user <replituser@example.com>
2021-02-28 05:26:43 +0000

    Added code to demonstrate the use of an if..else statement.

---
+++ b/de/s_d/if_else.py
@@ -0,0 +1,12 @@
+# Ask user for activity
+print("Please enter the activity to be performed.")
+activity = input()
+
+# declare function if_else
+if (activity == "calculate"):
+  print("\nPerforming calculations...")
+else:
+  print("\nPerforming activity...")
+
+# Display final message
+print("\nActivity completed!")
