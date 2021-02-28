cb4c6eda1cf5f31a3b727336789d46407ce1786e
repl.it user <replituser@example.com>
2021-02-28 05:20:54 +0000

    Added code to demonstrate the use of an if statement.

---
+++ b/de/s_d/if.py
@@ -0,0 +1,10 @@
+# ask user for type of book
+print("What type of book is this?")
+book_type = input()
+
+# apply decision if
+if (book_type == "adventure"):
+  print("\nI like adventure books!")
+
+# apply final message
+print("\nFinished reading book.")
